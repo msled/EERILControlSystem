49c7aee479d682a7e3c12961f93abff84c44f070
Dominic Chen <d.c.ddcc@gmail.com>
2012-02-03 00:25:09 -0700

    Properly remove BayerPattern.cs, ImageFormat.cs, and Interface.cs. Also add ColorTransformation.cs

---
+++ ERRI.ControlSystem/Avt/ColorTransformation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PvNET;
+
+namespace EERIL.ControlSystem.Avt
+{
+    public enum ColorTransformationMode : uint
+    {
+        Off = 0,
+        Manual = 1,
+        Temp5600K = 2
+    };
+
+    public class ColorTransformation
+    {
+        public ColorTransformationMode Mode;
+        public float ValueBB;
+        public float ValueBG;
+        public float ValueBR;
+        public float ValueGB;
+        public float ValueGG;
+        public float ValueGR;
+        public float ValueRB;
+        public float ValueRG;
+        public float ValueRR;
+    }
+}
